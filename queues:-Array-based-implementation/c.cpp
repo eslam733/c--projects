a48d4f0c219c3e789c3065a64d722c712bc991fe
eslam733 <emohamef733@gmail.com>
2019-07-16 23:49:00 +0200

    add new project

---
+++ queues:-Array-based-implementation/c.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define maxqueue 6
+#define queueEntry int
+#include "queue.h"
+void print(queueEntry e)
+{
+    cout << e << endl;
+}
+int main(void)
+{
+    queue q; int e;
+    createqueue(&q);
+    for (int i = 0; !(queuefull(&q)); i++)
+    {
+        push(i, &q);
+    }
+    
+    qprint(&q, &print);
+    return 0;
+}
---
+++ queues:-Array-based-implementation/queue.h
@@ -0,0 +1,62 @@
+#include <iostream>
+
+typedef struct Queue
+{
+    int front;
+    int rear;
+    int size;
+    queueEntry entry[maxqueue];
+}queue;
+
+void createqueue(queue *pq)
+{
+    pq->front = 0;
+    pq->rear = -1;
+    pq->size = 0;
+}
+
+void push(queueEntry e, queue *pq)
+{
+    pq->rear = (pq->rear + 1) % maxqueue;
+    pq->entry[pq->rear] = e;
+    pq->size++;
+}
+
+void pop(queueEntry *pe, queue *pq)
+{
+    *pe = pq->entry[pq->front];
+    pq->front = (pq->front + 1) % maxqueue;
+    pq->size--;
+}
+
+bool queueempty(queue *pq)
+{
+    return (pq->size == 0);
+}
+
+bool queuefull(queue *pq)
+{
+    return (pq->size == maxqueue);
+}
+
+int queuesize(queue *pq)
+{
+    return pq->size;
+}
+
+void queueclear(queue *pq)
+{
+    pq->front = 0;
+    pq->rear = -1;
+    pq->size = 0;
+}
+
+void qprint(queue *pq, void(*pf)(queueEntry))
+{
+    int pos = 0;
+    for (pos; pos < pq->size; pos++)
+    {
+        (*pf)(pq->entry[pos]);
+    }
+    
+}
